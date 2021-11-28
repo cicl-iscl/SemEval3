8303b7d47d472bfe69042997094cd3222db8c60e
Klara <klara.lennermann@student.uni-tuebingen.de>
2021-11-28 13:43:11 +0100

    make bert with tf work

--- BertSubtask1.py
+++ BertSubtask1.py
@@ -1,10 +1,15 @@
 import csv
+
+import numpy as np
 import tensorflow as tf
 import tensorflow_hub as hub
 import tensorflow_text as text
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
 from sklearn.model_selection import train_test_split
-from typing import List, Tuple
+from typing import List, Tuple, Dict
+
+from tensorflow.python.keras.callbacks import EarlyStopping
 
 
 def read_binary_label_data(filename: str, sentences: List[str], labels: List[int]):
@@ -40,7 +45,7 @@ def load_subtask1_data(language: str) -> Tuple[List[str], List[int], List[str], 
     return x_train, x_test, y_train, y_test
 
 
-def build_model(preprocessor: str, bert: str):
+def build_model(preprocessor: str, bert: str) -> tf.keras.Model:
     """
     Builds a binary classifier based on the pretrained models.
 
@@ -53,19 +58,58 @@ def build_model(preprocessor: str, bert: str):
     encoder_inputs = preprocessing_layer(text_input)
     encoder = hub.KerasLayer(bert, name="BERT")
     outputs = encoder(encoder_inputs)
-    net = outputs['pooled_output']
-    net = tf.keras.layers.Dropout(0.1)(net)
-    net = tf.keras.layers.Dense(1, activation=None, name='classifier')(net)
+    net = outputs["pooled_output"]
+    # net = tf.keras.layers.Dropout(0.05)(net)
+    net = tf.keras.layers.Dense(1, activation="sigmoid", name="classifier")(net)
     return tf.keras.Model(text_input, net)
+
+
+def train(model: tf.keras.Model, x_train: np.ndarray, y_train: np.ndarray):
+    """
+    Compiles and trains the given model.
+
+    :param model: Model to be trained
+    :param x_train: Training sentences
+    :param y_train: Training labels
+    """
+    model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
+    classifier.fit(x_train, y_train)
+
+
+def predict(model: tf.keras.Model, x_test: np.ndarray) -> List[int]:
+    """
+    Predicts binary labels.
+
+    :param model: The model the predictions are based on
+    :param x_test: The sentences to predict from
+    :return: List of predicted labels
+    """
+    predictions = model.predict(x_test)
+    return [1 if pr >= 0.5 else 0 for pr in predictions]
+
+
+def score(gold_labels: List[int], pred_labels: List[int]) -> Dict[str, float]:
+    """
+    
+    :param gold_labels: 
+    :param pred_labels: 
+    :return: 
+    """
+    return {'Accuracy': accuracy_score(gold_labels, pred_labels),
+            'Precision': precision_score(gold_labels, pred_labels),
+            'Recall': recall_score(gold_labels, pred_labels),
+            'F1': f1_score(gold_labels, pred_labels, average='binary'),
+            'F1 macro': f1_score(gold_labels, pred_labels, average='macro')}
 
 
 if __name__ == "__main__":
     # ENGLISH
     en_X_train, en_X_test, en_y_train, en_y_test = load_subtask1_data("en")
-    classifier = build_model("https://tfhub.dev/tensorflow/bert_en_cased_preprocess/3",
-                             "https://tfhub.dev/tensorflow/bert_en_cased_L-12_H-768_A-12/3")
-    classifier.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
-    # classifier.fit()
+    classifier = build_model("https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3",
+                             "https://tfhub.dev/tensorflow/bert_en_uncased_L-12_H-768_A-12/3")
+    train(classifier, np.array(en_X_train), np.array(en_y_train))
+    predicted_labels = predict(classifier, np.array(en_X_test))
+    print(score(en_y_test, predicted_labels))
 
     # Load French language data
     fr_X_train, fr_X_test, fr_y_train, fr_y_test = load_subtask1_data("fr")
